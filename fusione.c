2222928873af40a47247f881b8332b4fbc9d63d7
zioIgna <ignaziocarbonaro@hotmail.com>
2017-04-07 18:24:43 +0200

    Aggiunta funzioni

--- fusione.c
+++ fusione.c
@@ -23,13 +23,38 @@ void buildMinHeap(int vett[], int dim){
 
 void minHeapify(int vett[], int pos, int dim){
     int fsx, fdx, min, temp;
-    fsx=(pos+1)*2;
-    fdx=(pos+1)*2+1;
+    fsx=pos*2+1;
+    fdx=pos*2+2;
     if(fsx<dim && vett[fsx]<vett[pos])
         min=fsx;
     else
         min=pos;
     if(fdx<dim && vett[fdx]<vett[min])
+        min=fdx;
+    if(pos!=min){
+        temp=vett[pos];
+        vett[pos]=vett[min];
+        vett[min]=temp;
+        minHeapify(vett, min, dim);
+    }
+}
+
+void buildMinHeapList(Lista vett[], int pos, int dim){
+    int i;
+    for(i=dim/2; i>=0; i--)
+        minHeapifyList(vett, i, dim);
+}
+
+void minHeapifyList(Lista vett[], int pos, int dim){
+    int fsx, fdx, min;
+    Lista temp;
+    fsx=pos*2+1;
+    fdx=pos*2+2;
+    if(fsx<dim && vett[fsx]->val<vett[pos]->val)
+        min=fsx;
+    else
+        min=pos;
+    if(fdx<dim && vett[fdx]->val<vett[min]->val)
         min=fdx;
     if(pos!=min){
         temp=vett[pos];
